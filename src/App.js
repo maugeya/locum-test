980985b243ab9fce50326370aa2bee2eeb62faa8
Angela Maugey <a.maugey@gmail.com>
2020-09-17 16:37:46 +0100

    Adding LocumShiftList component to app passing props of current locum

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import LocumShiftList from "./components/LocumShiftList"
+
+const locum = {
+  id: "1234",
+  firstName: "John",
+  lastName: "Doe",
+  staffType: "gp",
+  staffTypeId: "1",
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>List of available locum shifts</h2>
+      <LocumShiftList locum={locum} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
