2ae3d715d9e4cfab3658419a61a417aa3fb4c30a
Angela Maugey <a.maugey@gmail.com>
2020-09-17 17:08:24 +0100

    Building out LocumShift card, adding basic styles

--- src/components/LocumShift.js
+++ src/components/LocumShift.js
@@ -1,7 +1,26 @@
 import React from "react"
+import moment from "moment"
+
+import styles from "./LocumShift.module.css"
 
 const LocumShift = ({ locumShift }) => {
-  return <div>{locumShift.startDatetime}</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <h2 className={styles.cardHeader}>{locumShift.practice.name}</h2>
+      </div>
+      <div className={styles.shiftDetails}>
+        <div>
+          Date: {moment(locumShift.startDatetime).format("MMMM do YYYY")}
+        </div>
+        <div>
+          Start Time: {moment(locumShift.startDatetime).format("HH:mm")}
+        </div>
+        <div>End Time: {moment(locumShift.endDatetime).format("HH:mm")}</div>
+        <div>Hourly Rate: £{locumShift.hourlyRate}</div>
+      </div>
+    </div>
+  )
 }
 
 export default LocumShift
