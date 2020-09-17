768b08de4358518d3e639e92e5302e119a96da6d
Angela Maugey <a.maugey@gmail.com>
2020-09-17 17:53:42 +0100

    Really basic test on App.test.js to avoid test failing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders a header for the page", () => {
+  const { getByText } = render(<App />)
+  const pageHeader = getByText(/Available locum shifts/i)
+  expect(pageHeader).toBeInTheDocument()
+})
